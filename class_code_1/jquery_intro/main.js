f5cd97d311558f6528df4e0475d6a1f9219d2ba0
SarahBurnettColum <71103410+SarahBurnettColum@users.noreply.github.com>
2020-11-17 21:58:36 -0600

    new 11/17/20

---
+++ class_code_1/jquery_intro/main.js
@@ -0,0 +1,7 @@
+console.log("Hey! How are you?")
+
+$('.box').click(function(){
+    console.log("Don't click me!")
+$(".box").css("width", "200px");
+
+})
---
+++ class_code_1/jquery_modal_popout/main.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn.onclick = function() {
+  modal.style.display = "none";
+}
